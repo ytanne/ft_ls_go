33f026ddc1729e959fe9b9921c1bce74b9ad90d5
fl4ssh <fl4ssh>
2021-07-26 21:13:15 +0600

    Added including entries

--- ls/ls.go
+++ ls/ls.go
@@ -14,9 +14,7 @@ const (
 )
 
 type Flags struct {
-	Dir                  string
-	NoFlags              bool
-	IgnoreEntries        bool
+	IncludeEntries       bool
 	Recursive            bool
 	LongListingFormat    bool
 	ReverseSort          bool
@@ -28,30 +26,69 @@ func Help() {
 }
 
 func ProcessArgs(args ...string) {
-	f := new(Flags)
-	if len(args) == 1 {
-		f.NoFlags = true
-		f.IgnoreEntries = true
-		f.Dir = "."
+	f := &Flags{}
+	var directories []string
+
+	for _, arg := range args[1:] {
+		if arg[0] == '-' {
+			if len(arg) == 1 {
+				panic("Cannot open directory '-'")
+			}
+			for _, letter := range arg[1:] {
+				switch letter {
+				case 'l':
+					f.LongListingFormat = true
+				case 'a':
+					f.IncludeEntries = true
+				case 'R':
+					f.Recursive = true
+				case 'r':
+					f.ReverseSort = true
+				case 't':
+					f.ModificationTimeSort = true
+				default:
+					fmt.Printf("flag -%v is not implemented yet", letter)
+				}
+			}
+		} else {
+			directories = append(directories, arg)
+		}
 	}
-	if err := PrintDirectory(f); err != nil {
-		panic(err)
+
+	if len(directories) == 0 {
+		PrintDirectory(f, ".")
+		return
+	}
+
+	for _, dir := range directories {
+		if err := PrintDirectory(f, dir); err != nil {
+			panic(err)
+		}
 	}
 }
 
-func PrintDirectory(f *Flags) (err error) {
-	var dirContent []fs.DirEntry
-	if f.NoFlags {
-		dirContent, err = getDirContent(f.Dir, alphabeticOrder)
-	}
+func PrintDirectory(f *Flags, dir string) error {
+	var directories []string
+	dirContent, err := getDirContent(dir, alphabeticOrder)
 	if err != nil {
 		return err
 	}
+
+	if f.IncludeEntries {
+		fmt.Printf(Purple("."))
+		fmt.Print("  ")
+		fmt.Printf(Purple(".."))
+		fmt.Print("  ")
+	}
+
 	for i, dir := range dirContent {
-		if f.IgnoreEntries && dir.Name()[0] == '.' {
+		if !f.IncludeEntries && dir.Name()[0] == '.' {
 			continue
 		}
 		if dir.Type().IsDir() {
+			if f.Recursive {
+				directories = append(directories, dir.Name())
+			}
 			fmt.Printf(Purple(dir.Name()))
 		} else {
 			fmt.Printf("%s", dir.Name())
@@ -62,6 +99,11 @@ func PrintDirectory(f *Flags) (err error) {
 			fmt.Println()
 		}
 	}
+
+	for _, intDir := range directories {
+		PrintDirectory(f, intDir)
+	}
+
 	return nil
 }
 
@@ -75,7 +117,7 @@ func getDirContent(dir string, order int) ([]fs.DirEntry, error) {
 		return nil, err
 	}
 	switch order {
-	case 0:
+	case alphabeticOrder:
 		sort.Slice(dirContent, func(i, j int) bool {
 			var a, b = dirContent[i].Name()[0], dirContent[j].Name()[0]
 			if a >= 'A' && a <= 'Z' {
